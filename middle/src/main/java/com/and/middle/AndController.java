20522feae12d3476f8956622ee26ae200e2edc8a
wonhee <asdkcone13@gmail.com>
2022-12-13 16:56:52 +0900

    andVo Gson으로 보내기

--- middle/src/main/java/com/and/middle/AndController.java
+++ middle/src/main/java/com/and/middle/AndController.java
@@ -1,15 +1,41 @@
 package com.and.middle;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+
+import customer.CustomerVO;
 
 @RestController		// 안되면 spring-framework 버전 확인 (낮으면 안됨)
 public class AndController {
 	
+	@Autowired @Qualifier("hanul") SqlSession sql;
+	
 	// @RestController 안에서는 생략가능!
-	@RequestMapping("/and")
+	@RequestMapping(value="/and", produces = "text/html;charset=utf-8")
 	public String and() {
-		return "testㄷㄴㅅ";
+		
+		List<CustomerVO> list = sql.selectList("and.list");
+		
+		for(int i = 0; i < list.size(); i++) {
+			System.out.print(list.get(i).getName() + " / " + list.get(i).getGender() +"\n");
+		}
+		return new Gson().toJson(list);
+	}
+	
+	@RequestMapping(value="/andVo", produces = "text/html;charset=utf-8")
+	public String andVo() {
+		
+		List<CustomerVO> list = sql.selectList("and.list");
+		
+		
+		return new Gson().toJson(list.get(0));
 	}
 	
 }
